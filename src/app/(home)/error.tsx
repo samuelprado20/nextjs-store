d3b067a264e742a250de32d33f11816e5681fdb6
Samuel Prado <samuel.prado.osorio20@gmail.com>
2024-01-17 22:01:20 -0500

    Add Home error page

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+interface ErrorProps {
+  error: Error
+  reset: () => void
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return(
+    <div style={{
+      width: '100%',
+      height: '300px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <p style={{fontWeight: 'bolder', fontSize: '2rem'}}>Something went wrong :c</p>
+      <button
+        onClick={reset}
+        style={{
+          height: '50px',
+          width: '95px',
+          border: 'none',
+          borderRadius: '6px',
+          backgroundColor: '#551a8b',
+          color: '#e4e1f6',
+          fontSize: '1rem'
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
