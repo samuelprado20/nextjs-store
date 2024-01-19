e98f92778eeccc491ed13c53743d94f2e3bcb2f2
Samuel Prado <samuel.prado.osorio20@gmail.com>
2024-01-18 22:04:50 -0500

    update file architecture for data fetching

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,19 +1,7 @@
 import Image from "next/image"
 import styles from './MainProducts.module.sass'
+import { getProducts } from '@/services/shopify'
 
-const getProducts = async () => {
-  try{
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`, {
-      headers: new Headers({
-        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ''
-      })
-    })
-    const {products} = await response.json()
-    return products
-  } catch (error) {
-    console.log(error)
-  }
-}
 
 export const MainProducts = async () => {
   const products = await getProducts()
