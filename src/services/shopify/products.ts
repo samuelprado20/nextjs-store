842046053c5011a6b0d544305909cd73dc852c05
Samuel Prado <samuel.prado.osorio20@gmail.com>
2024-02-07 21:58:35 -0500

    Cache, revalidateTag and revalidatePath

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -28,3 +28,18 @@ export const getProducts = async (id?: string): Promise<ProductType[]> => {
     console.log(error)
   }
 }
+export const getMainProducts = async () => {
+  const response = await fetch(shopifyUrls.products.mainProducts, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+    }),
+    cache: 'force-cache',
+    next: {
+      tags: ['main-products']
+    }
+  })
+
+  const {products} = await response.json()
+
+  return products
+}
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,7 +2,8 @@ import { env } from '@/config/env'
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`,
+    mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/collections/471364337966/products.json`
   },
   collections: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/smart_collections.json`,
