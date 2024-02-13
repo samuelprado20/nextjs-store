ac9eef37d90d4de74c32d6a733542fd085d59c7e
Samuel Prado <samuel.prado.osorio20@gmail.com>
2024-02-12 22:53:17 -0500

    redirect for server and client components

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductView } from "@/components/product/ProductView"
 import { getProducts } from "@/services/shopify/products"
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
   searchParams: {
@@ -10,6 +11,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id
   const products = await getProducts(id)
   const product = products[0]
+
+  if(!id) {
+    redirect('/store')
+  }
 
   return <ProductView product={product}/>
 }
