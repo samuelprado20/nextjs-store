46be2cac3665728f7586f1cddd520937cabd425d
Samuel Prado <samuel.prado.osorio20@gmail.com>
2024-02-18 04:55:10 -0500

    SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next'
 import { MainProducts } from '../../components/home/MainProducts'
 
+
+export const metadata: Metadata = {
+  title: '✨ Future world',
+  description: 'Welcome to the future world, a futuristic store for all your needs',
+  keywords: ['ecommerce', 'future', 'world', 'technology']
+}
 export default function Home() {
   return (
     <main >
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -7,6 +7,22 @@ interface ProductPageProps {
     id: string
   }
 }
+
+export async function generateMetadata({ searchParams}: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image]
+    }
+  }
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id
   const products = await getProducts(id)
